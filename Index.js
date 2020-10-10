fb3d8934bb8d833c1066b94f49534a3a4316933b
Britt Ralph <brittney.mr22@gmail.com>
2020-10-10 11:58:47 -0400

    modified

---
+++ Index.js
@@ -0,0 +1,56 @@
+const inquirer = require('inquirer');
+
+
+class Employee{
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+  
+    printStats() {
+      console.log(
+        'Name: ' +
+          this.name +
+          '\nStrength: ' +
+          this.strength +
+          '\nHitPoints: ' +
+          this.hitpoints
+      );
+      console.log('\n-------------\n');
+    }
+  
+    isAlive() {
+      if (this.hitpoints > 0) {
+        console.log(this.name + ' is still alive!');
+        console.log('\n-------------\n');
+      } else {
+        console.log(this.name + ' has died!');
+      }
+    }
+  
+    attack(opponent) {
+      opponent.hitpoints -= this.strength;
+      console.log(this.name + ' attacked ' + opponent.name + '!!');
+      console.log('\n-------------\n');
+    }
+  }
+  
+  // =============================================================
+  
+  // Create two new instances of a `Character`, giving them different names, strength, and hitPoints.
+  let yogi = new Character('Dhalsim', 20, 65);
+  let mutant = new Character('Blanka', 30, 50);
+  
+  // Call a combination of `printStats()`, `attack()`, and `isAlive()` methods to have the two characters "fight" in your console.
+  yogi.printStats();
+  mutant.printStats();
+  
+  mutant.attack(yogi);
+  yogi.printStats();
+  yogi.isAlive();
+  
+  yogi.attack(mutant);
+  mutant.printStats();
+  mutant.isAlive();
+  
